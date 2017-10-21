699bf029f35488202389b19fd203be705b5826e6
Emelie Javelind <emelie@auddly.com>
2017-10-21 15:38:31 +0200

    Use same database for dev and prod 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+import dotenv from 'dotenv';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
-  require('dotenv').config({ path: '.env.test' });
+  dotenv.config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: '.env.development' });
+  dotenv.config({ path: '.env.development' });
 } else if (process.env.NODE_ENV === 'production') {
-  require('dotenv').config({ path: '.env.development' });
+  dotenv.config({ path: '.env.development' });
 }
 
 module.exports = (env) => {
